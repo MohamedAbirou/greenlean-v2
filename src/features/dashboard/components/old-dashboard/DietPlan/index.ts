96b3a685b0719d36cdd85db0a761c6412bfdcea4
Mohamed Abirou <abiroumohamed58@gmail.com>
2025-12-05 17:07:36 +0100

    some tweaks

--- src/features/dashboard/components/old-dashboard/DietPlan/index.ts
+++
@@ -1,8 +0,0 @@
-export * from './HydrationPanel';
-export * from './MealList';
-export * from './MealLogModal';
-export * from './MealTabs';
-export * from './ProgressCards';
-export * from './ShoppingPanel';
-export * from './TipsPanel';
-
